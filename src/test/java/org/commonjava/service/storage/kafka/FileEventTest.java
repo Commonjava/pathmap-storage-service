cea0a9395184dd58b0c453b1f6c25cec98f09b5b
Wenjie Guo <wguo@redhat.com>
2021-02-25 10:50:49 +0800

    Using the in-memory connector for kafka messaging test

---
+++ src/test/java/org/commonjava/service/storage/kafka/FileEventTest.java
@@ -0,0 +1,53 @@
+package org.commonjava.service.storage.kafka;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.reactive.messaging.connectors.InMemoryConnector;
+import io.smallrye.reactive.messaging.connectors.InMemorySink;
+import io.smallrye.reactive.messaging.connectors.InMemorySource;
+import org.commonjava.service.storage.event.FileEvent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+
+@QuarkusTest
+@QuarkusTestResource(KafkaTestResourceLifecycleManager.class)
+public class FileEventTest
+{
+
+    @Inject @Any
+    InMemoryConnector connector;
+
+    @Test
+    void testConsumeFileEvent()
+    {
+        InMemorySource<FileEvent> events = connector.source( "file-event-in");
+
+        FileEvent fileEvent = new FileEvent();
+        fileEvent.setTrackingID( "build-0001" );
+
+        // Use the send method to send a mock message to the events channel. So, our application will process this message.
+        events.send(fileEvent);
+
+        // TODO wait the app to process the event
+
+        // TODO verify the result after processing the event
+
+    }
+
+    @Test
+    void testProduceFileEvent()
+    {
+        InMemorySink<FileEvent> queue = connector.sink( "file-event-out");
+
+        // TODO trigger the application to send the message
+
+        // TODO
+        // Assertions.assertEquals( 1, queue.received().size() );
+        // FileEvent event = queue.received().get(0).getPayload();
+
+    }
+
+}
